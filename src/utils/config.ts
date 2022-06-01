9a8f480076ab9687bf10fed37b084b2695adbb4d
judegiordano <judegiordano@gmail.com>
2022-06-01 11:14:13 -0400

    Remove Unecessary Env

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,6 +1,5 @@
 // public
 export const NEXT_PUBLIC_DOMAIN = process.env.NEXT_PUBLIC_DOMAIN ?? "http://localhost:3000";
-export const NEXT_PUBLIC_DISCORD_API = "https://discord.com/api/v8";
 export const NEXT_PUBLIC_COOKIE_NAME = "billybot.dashboard.jid";
 // private
 export const BACKEND_API = process.env.BACKEND_API as string;
